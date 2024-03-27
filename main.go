4f3f194e7440899188ba5883753c56bd02d26453
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-28 04:40:22 +0600

    Added code to upload a file to a Slack channel using the Slack API
    - The main function is now populated with code to set environment variables, initialize the Slack API client, define a channel array and a file array
    - A for loop is used to iterate over the file array and upload each file to the channel using the Slack API's UploadFile method
    - Error handling is included to log any errors that occur during the file upload process.

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
 func main() {
-	
+	os.Setenv("TOKEN", "token of the bot")
+	os.Setenv("CHANNEL_ID", "C04J7QW95U4")
+	api := slack.New(os.Getenv("TOKEN"))
+	channeArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"book.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channeArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("file uploaded %v", file.Name, file.URL)
+	}
 }
